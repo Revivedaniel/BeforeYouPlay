54b7cb6ea3de7506a8e31e785e6f9ae6bbcd5967
Revivedaniel <mlg.danielstark@gmail.com>
2021-12-23 20:58:28 -0800

    Created game list component

---
+++ client/src/components/GameList.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+const GameImage = styled.img`
+  max-height: 150px;
+  width: 108px;
+`;
+
+export function GameList({ games, title }) {
+  if (!games.length) {
+    return <h3>No Games Yet</h3>;
+  }
+
+  return (
+    <div>
+      <h3>{title}</h3>
+      {games.map((game) => {
+        return (
+          <div key={game._id}>
+            <GameImage
+              src={`https://images.igdb.com/igdb/image/upload/t_1080p/${game.cover_id}.jpg`}
+              alt={"Video game cover art"}
+            />
+            <div>
+              <p>{game.title}</p>
+              <p>{game.release_year}</p>
+              <div>
+                {JSON.parse(game.genres).map((genre, i) => {
+                  return <p key={i}>{genre}</p>;
+                })}
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
