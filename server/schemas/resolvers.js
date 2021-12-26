10bfe6336fb7cb4c64ee1ec298b0083019d4a91f
Revivedaniel <mlg.danielstark@gmail.com>
2021-12-26 00:30:48 -0800

    the game query searches IGDB is game is not in our DB

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User, Game } = require('../models');
+const { User, Game } = require("../models");
+const axios = require("axios");
 
 const resolvers = {
   Query: {
@@ -15,20 +16,57 @@ const resolvers = {
     users: async () => {
       try {
         let user = await User.find({});
-        
+
         return user;
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     },
     game: async (parent, { slug }) => {
       try {
         //This will return an array so we return game[0] for the first entry
-        let game = await Game.find({ slug: slug })
+        let game = await Game.find({ slug: slug });
+        //If there is no game with that slug, search IGDB
+        if (game.length === 0) {
+          let response = await axios({
+            url: "https://api.igdb.com/v4/games",
+            method: "POST",
+            headers: {
+              Accept: "application/json",
+              "Client-ID": `${process.env.CLIENT_ID}`,
+              Authorization: `Bearer ${process.env.TOKEN}`,
+            },
+            data: `fields age_ratings.rating, cover.image_id, genres.name, name, slug, summary, release_dates.y; where slug = "${slug}";`,
+          });
+          const gameData = response.data[0];
+          // release date is the first release date
+          gameData.release_date = gameData.release_dates[0].y;
+          // genres is the name of the genre instead of the id and name
+          let genres = gameData.genres.map((genreObj) => {
+            return genreObj.name;
+          });
+          // age_rating is the ESRB rating name
+          gameData.age_rating = gameData.age_ratings[0].rating;
 
-        return game[0];
+          // New game for mongoose
+          let newGame = {
+            title: gameData.name,
+            summary: gameData.summary,
+            cover_id: gameData.cover.image_id,
+            release_year: gameData.release_date,
+            genres: JSON.stringify(genres),
+            age_rating: gameData.age_rating,
+            slug: gameData.slug,
+          };
+          //Creating the game in our database
+          game = await Game.create(newGame);
+          //Returning the new game
+          return game;
+        } else {
+          return game[0];
+        }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     },
     games: async () => {
@@ -36,10 +74,8 @@ const resolvers = {
         let games = await Game.find({});
 
         return games;
-      } catch (error) {
-        
-      }
-    }
+      } catch (error) {}
+    },
   },
 };
 
