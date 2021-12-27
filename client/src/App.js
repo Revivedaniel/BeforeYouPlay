88a59a59270385a83af615c5c1c7f34dc6c54e3e
Revivedaniel <mlg.danielstark@gmail.com>
2021-12-26 23:51:07 -0800

    if a token exists in local storage, save it as an authorization header

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { setContext } from '@apollo/client/link/context';
 
 import "./App.css";
 
@@ -11,9 +12,24 @@ import { Header } from "./components/Header";
 import { Homepage } from "./pages/Homepage";
 import { Gamepage } from "./pages/Gamepage";
 import { Searchpage } from "./pages/Searchpage";
+import { Login } from "./pages/Login";
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+      headers: {
+          ...headers,
+          authorization: token ? `Bearer ${token}` : '',
+      },
+  };
+});
 
 const client = new ApolloClient({
-  uri: "/graphql",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
@@ -27,6 +43,7 @@ function App() {
             <Route index path="/" element={<Homepage />} />
             <Route path="/games/:slug" element={<Gamepage />} />
             <Route path="/search/:search" element={<Searchpage />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </Router>
