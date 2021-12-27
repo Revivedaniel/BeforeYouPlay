5483121ab39ad1772f20ccf026049d2860d9cf1a
Revivedaniel <mlg.danielstark@gmail.com>
2021-12-26 17:44:54 -0800

    Search page displays 10 results at a time

--- client/src/pages/Searchpage.js
+++ client/src/pages/Searchpage.js
@@ -1,4 +1,24 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { GameList } from "../components/GameList";
+
+import { QUERY_SEARCH_GAME } from "../utils/queries";
+
 export function Searchpage() {
-    return <h1>This is the Searchpage</h1>;
-  }
-  
+  const { search } = useParams();
+  const { loading, data } = useQuery(QUERY_SEARCH_GAME, {
+    variables: { search: search },
+  });
+  const games = data?.searchGame || [];
+  console.log(games[0])
+
+  return (
+    <main>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <GameList games={games} title="Here  are the search results" />
+      )}
+    </main>
+  );
+}
