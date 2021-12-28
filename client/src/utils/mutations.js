a81693eced993d5b4a64e06f8b73db242f82bf56
Revivedaniel <mlg.danielstark@gmail.com>
2021-12-27 23:06:18 -0800

    addReview mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,33 +1,45 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const ADD_USER = gql`
-    mutation addUser($username: String!, $email: String!, $password: String!) {
-        addUser(username: $username, email: $email, password: $password) {
-            token
-            user {
-                _id
-            }
-        }
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
     }
+  }
 `;
 
 export const UPDATE_USER = gql`
-    mutation updateUser($username: String!, $email: String!, $password: String!) {
-        updateUser(username: $username, email: $email, password: $password) {
-            _id
-            username
-            email
-        }
+  mutation updateUser($username: String!, $email: String!, $password: String!) {
+    updateUser(username: $username, email: $email, password: $password) {
+      _id
+      username
+      email
     }
+  }
 `;
 
 export const LOGIN = gql`
-    mutation login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            token
-            user {
-                _id
-            }
-        }
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+      }
     }
+  }
+`;
+
+export const ADD_REVIEW = gql`
+  mutation addReview($game_id: ID!, $stars: Int!, $review_body: String!) {
+    addReview(game_id: $game_id, stars: $stars, review_body: $review_body) {
+      _id
+      user_id
+      game_id
+      stars
+      review_body
+    }
+  }
 `;
