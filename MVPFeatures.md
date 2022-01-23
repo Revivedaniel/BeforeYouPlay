fc2785a295feea407afdf722375faabd55eac1a5
Revivedaniel <mlg.danielstark@gmail.com>
2022-01-22 16:46:03 -0800

    Search buttons have hover, login, logout, and jumbotron style cleaned up

---
+++ MVPFeatures.md
@@ -0,0 +1,19 @@
+# Features for MVP
+
+1. Header
+    - Website title on the left, when clicked takes you to homepage
+    - search bar in middle, searche button and feeling lucky button
+    - user controls on the right (CLick to sign in if not logged in, if logged in then click for a drop down with options starting with log out)
+2. Homepage
+    - front page loades popular video games (Based on user interaction && release date || most recent game added to the database)
+    - video game cards display cover art, title, release date, genre
+    - video game cards open the page for that game when clicked.
+3. Individual Game Page
+    - At the top, Info about the game including, cover, summary, age rating, title, genre, average user rating, time to complete(Average)
+    - on the right, navigation for this games sections (To start will be about and reviews)
+    - the first component to develop is the ability to leave a review
+    - When leaving a review, you select how many stars you give the game and leave a message as the review
+4. Search page
+    - display the first 10 results
+    - As long as the response from igdb has more than 10 entries, create next buttons that display the next 10 entries in the list and so on.
+    - Game cards will be the same as the homepage game cards to start and when clicked take you to the individual game page.
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -15,12 +15,23 @@ const UserPortal = styled.div`
     background-color: rgba(196, 196, 196, 1);
     margin-right: 2%;
   } */
+  a {
+    text-decoration: none;
+    font-size: 2rem;
+    color: rgb(255, 255, 255);
+    line-height: 1;
+    cursor: pointer;
+    padding: 0.7rem 1rem;
+  }
 `;
 
 // Jumbotron
 const Jumbotron = styled.h1`
   width: 33.33%;
   a {
+    color: rgb(255,255,255);
+    cursor: pointer;
+    padding: 0.7rem 1rem;
     text-decoration: none;
     font-family: Roboto;
     font-style: normal;
@@ -30,7 +41,6 @@ const Jumbotron = styled.h1`
     display: flex;
     align-items: center;
     text-align: center;
-    color: white;
   }
 `;
 
@@ -41,11 +51,9 @@ const SearchBar = styled.span`
   display: flex;
   align-items: center;
   a {
-    border-color: var(--ytd-searchbox-legacy-button-hover-border-color);
     background-color: white;
     box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 0px;
     outline: none;
-    border: 1px solid var(--ytd-searchbox-legacy-button-border-color);
     border-radius: 0px 2px 2px 0px;
     cursor: pointer;
     height: 40px;
@@ -54,6 +62,9 @@ const SearchBar = styled.span`
     display: flex;
     justify-content: center;
     align-items: center;
+    &:hover {
+      background-color: lightgrey;
+    }
   }
   input {
     font-size: 14px;
@@ -103,17 +114,28 @@ export function Header() {
           onChange={handleInputChange}
           placeholder="Search for a game..."
         />
-        <Link to={`/search/${encodeURI(search)}`} style={{borderTopRightRadius: "0", borderBottomRightRadius: "0"}}>
+        <Link
+          to={`/search/${encodeURI(search)}`}
+          style={{ borderTopRightRadius: "0", borderBottomRightRadius: "0" }}
+        >
           <svg
             viewBox="0 0 24 24"
             preserveAspectRatio="xMidYMid meet"
             focusable="false"
-            style={{pointerEvents: "none", display: "block", width: "50%", height: "50%"}}
+            style={{
+              pointerEvents: "none",
+              display: "block",
+              width: "50%",
+              height: "50%",
+            }}
           >
             <path d="M20.87,20.17l-5.59-5.59C16.35,13.35,17,11.75,17,10c0-3.87-3.13-7-7-7s-7,3.13-7,7s3.13,7,7,7c1.75,0,3.35-0.65,4.58-1.71 l5.59,5.59L20.87,20.17z M10,16c-3.31,0-6-2.69-6-6s2.69-6,6-6s6,2.69,6,6S13.31,16,10,16z"></path>
           </svg>
         </Link>
-        <Link to={`/games/${search.replace(/\s+/g, "-").toLowerCase()}`}>
+        <Link
+          to={`/games/${search.replace(/\s+/g, "-").toLowerCase()}`}
+          style={{ color: "green" }}
+        >
           777
         </Link>
       </SearchBar>
@@ -122,7 +144,6 @@ export function Header() {
           <Link
             id="login"
             to="/login"
-            style={{ textDecoration: "none", fontSize: "2rem" }}
           >
             Login
           </Link>
@@ -132,7 +153,6 @@ export function Header() {
               id="logout"
               to="/"
               onClick={() => Auth.logout()}
-              style={{ textDecoration: "none", fontSize: "2rem" }}
             >
               Logout
             </Link>
