03498aed283c02a6b7f9ba37707401a9da3a0e48
Revivedaniel <mlg.danielstark@gmail.com>
2022-02-12 19:19:58 -0800

    Styled the slider slides to be standard size

--- client/src/components/MovieSlider.js
+++ client/src/components/MovieSlider.js
@@ -37,37 +37,13 @@ export default function MovieSlider() {
               <i className="ion-social-youtube"></i>
             </a>
           </div>
-          <div className="slick-multiItemSlider">
-            {games.map((game) => {
+          <div className="slick-multiItemSlider" style={{ display: "flex" }}>
+            {games.map((game, i) => {
+              if (i < 4) {
+                return;
+              }
               return <SliderCard game={game} key={game._id} />;
             })}
-
-            <div class="movie-item">
-              <div class="mv-img">
-                <a href="#">
-                  <img
-                    src="images/uploads/slider1.jpg"
-                    alt=""
-                    width="285"
-                    height="437"
-                  />
-                </a>
-              </div>
-              <div class="title-in">
-                <div class="cate">
-                  <span class="blue">
-                    <a href="#">Sci-fi</a>
-                  </span>
-                </div>
-                <h6>
-                  <a href="#">Interstellar</a>
-                </h6>
-                <p>
-                  <i class="ion-android-star"></i>
-                  <span>7.4</span> /10
-                </p>
-              </div>
-            </div>
           </div>
         </div>
       </div>
--- client/src/components/partials/SliderCard.js
+++ client/src/components/partials/SliderCard.js
@@ -1,27 +1,28 @@
 export default function SliderCard({ game }) {
   return (
-    <div class="movie-item">
-      <div class="mv-img">
+    <div className="movie-item" style={{minWidth: "285px", height: "437px"}}>
+      <div className="mv-img">
         <a href="#">
           <img
-            src="images/uploads/slider1.jpg"
-            alt=""
+            src={`https://images.igdb.com/igdb/image/upload/t_1080p/${game.cover_id}.jpg`}
+            alt={`${game.title} cover art`}
             width="285"
             height="437"
+            style={{ width: "285px", height: "437px" }}
           />
         </a>
       </div>
-      <div class="title-in">
-        <div class="cate">
-          <span class="blue">
+      <div className="title-in">
+        <div className="cate">
+          <span className="blue">
             <a href="#">Sci-fi</a>
           </span>
         </div>
         <h6>
-          <a href="#">Interstellar</a>
+          <a href="#">{game.title}</a>
         </h6>
         <p>
-          <i class="ion-android-star"></i>
+          <i className="ion-android-star"></i>
           <span>7.4</span> /10
         </p>
       </div>
