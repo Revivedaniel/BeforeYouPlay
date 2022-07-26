107691587e753b51cc78ea490e35053f7ec7e59f
TBelmont2 <85715276+TBelmont2@users.noreply.github.com>
2022-07-26 19:19:58 -0400

    updated models

---
+++ server/models/FreshData.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const freshDataSchema = new Schema({
+    game_id:{
+        type: Schema.Types.ObjectId,
+    },
+    created_at: {
+        type: Date,
+    },
+    up_votes: {
+        type: Number,
+    },
+    down_votes: {
+        type: Number,
+    },
+    potentially_outdated: {
+        type: Boolean,
+    },
+    data: {
+        type: String,
+    },
+    data_title: {
+        type: String,
+    },
+    admin_approvals: {
+        type: Number,
+    },
+    votes_total: {
+        type: String,
+    },
+    manually_typed: {
+        type: Boolean,
+    },
+})
+
+const FreshData = mongoose.model('FreshData', freshDataSchema);
+
+module.exports = FreshData;
