f80b87b4258c0a01de9f05b864e5f1a68f1d46d6
Revivedaniel <mlg.danielstark@gmail.com>
2022-08-10 19:39:21 -0700

    Catching up to Tyler

--- server/models/Review.js
+++ server/models/Review.js
@@ -10,9 +10,24 @@ const reviewSchema = new Schema({
     game_id: {
         type: Schema.Types.ObjectId, 
     },
+    username: {
+        type: String,
+        require: true, 
+    },
+    game_id: {
+        type: Schema.Types.ObjectId, 
+    },
+    // title: {
+    //     type: String,
+    //     maxlength: 70,
+    // },
     stars: {
         type: Number
-    }
+    },
+    // review_body: {
+    //     type: String,
+    //     maxLength: 450
+    // }
 });
 
 const Review = mongoose.model('Review', reviewSchema);
