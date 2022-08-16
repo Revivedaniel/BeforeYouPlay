c465bdced63a8dabe17ebb8d89b7d482df30127f
Revivedaniel <mlg.danielstark@gmail.com>
2022-08-15 18:40:21 -0700

    gameTeam splits into seperate datapoints

--- server/utils/createFreshData.js
+++ server/utils/createFreshData.js
@@ -9,6 +9,7 @@ function createFreshData(game) {
       // This helps only evaluate the data in the mongoos object
       if (typeof game[key] !== "string") continue;
       if (key === "custom_datapoints") continue;
+      if (key === "id") continue;
 
       let data;
       if (typeof game[key] !== "string") {
@@ -32,18 +33,36 @@ function createFreshData(game) {
     }
     // for each datapoint in custom_datapoints, create a new fresh data object and add it to freshData
     for (let key in freshDataObj) {
-      freshData.push({
-        game_id: game._id,
-        created_at: new Date(),
-        up_votes: 0,
-        down_votes: 0,
-        potentially_outdated: false,
-        data: JSON.stringify(freshDataObj[key]),
-        data_title: key,
-        admin_approvals: 0,
-        votes_total: 0,
-        manually_typed: false,
-      });
+      // if the key is gameTeam, then add each element in the gameTeam object to freshData
+      if (key === "gameTeam") {
+        for (let key2 in freshDataObj[key]) {
+          freshData.push({
+            game_id: game._id,
+            created_at: new Date(),
+            up_votes: 0,
+            down_votes: 0,
+            potentially_outdated: false,
+            data: JSON.stringify(freshDataObj[key][key2]),
+            data_title: key2,
+            admin_approvals: 0,
+            votes_total: 0,
+            manually_typed: false,
+          });
+        }
+      } else {
+        freshData.push({
+          game_id: game._id,
+          created_at: new Date(),
+          up_votes: 0,
+          down_votes: 0,
+          potentially_outdated: false,
+          data: JSON.stringify(freshDataObj[key]),
+          data_title: key,
+          admin_approvals: 0,
+          votes_total: 0,
+          manually_typed: false,
+        });
+      }
     }
     await FreshData.insertMany(freshData);
     resolve();
