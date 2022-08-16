8ee3028220b15c82a6d9dd6d25eeeb4283cb1194
Revivedaniel <mlg.danielstark@gmail.com>
2022-08-15 18:39:29 -0700

    Will now dig into game team to find datapoint if dataType is GameTeam

--- server/utils/updateCustomDatapoints.js
+++ server/utils/updateCustomDatapoints.js
@@ -1,12 +1,22 @@
-function updateCustomDatapoints(custom_datapoints, datapointTitle, newData) {
-    // if custom_datapoints is a string, convert it to an object
-    // find the datapoint with the datapointTitle and update it with the newData
-    // return the updated custom_datapoints object
-    if (typeof custom_datapoints === "string") {
-        custom_datapoints = JSON.parse(custom_datapoints);
-    }
-    custom_datapoints[datapointTitle] = newData;
+function updateCustomDatapoints(
+  custom_datapoints,
+  datapointTitle,
+  newData,
+  dataType
+) {
+  // if custom_datapoints is a string, convert it to an object
+  // find the datapoint with the datapointTitle and update it with the newData
+  // return the updated custom_datapoints object
+  if (typeof custom_datapoints === "string") {
+    custom_datapoints = JSON.parse(custom_datapoints);
+  }
+  if (dataType === "GameTeam") {
+    custom_datapoints.gameTeam[datapointTitle] = newData;
+    console.log(custom_datapoints);
     return JSON.stringify(custom_datapoints);
+  }
+  custom_datapoints[datapointTitle] = newData;
+  return JSON.stringify(custom_datapoints);
 }
 
 module.exports = updateCustomDatapoints;
