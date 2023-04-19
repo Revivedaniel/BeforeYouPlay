18baced53040f35a7212a52f5f8c4d6dfeb6ffa0
Revivedaniel <mlg.danielstark@gmail.com>
2023-04-18 17:01:37 -0700

    Better loading UI for gamepage

--- client/src/components/Gamepage/GameImage.tsx
+++ client/src/components/Gamepage/GameImage.tsx
@@ -6,17 +6,25 @@ interface Game {
 }
 
 interface GameImageProps {
-  game: Game;
+  game?: Game;
 }
 
 export default function GameImage(props: GameImageProps) {
   return (
     <>
-      <img
+      {props.game ? (
+        <img
         src={`https://vgiapitest.blob.core.windows.net/game-images/${props.game.imageName}.webp`}
         alt={`Cover art for the video game ${props.game.title}`}
         className={css.img}
       />
+      ) : (
+        <img
+        src={`https://vgiapitest.blob.core.windows.net/game-images/byp-new-game.webp`}
+        alt={"Generic cover art which displays when a game is loading or the game has not been reviewed by an admin."}
+        className={css.img}
+      />
+      )}
     </>
   );
 }
