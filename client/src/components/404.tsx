c1701eb290faacd99588166e7af8926328b69de5
Revivedaniel <mlg.danielstark@gmail.com>
2023-05-07 09:35:46 -0700

    Optimized Images using Next/Image

--- client/src/components/404.tsx
+++ client/src/components/404.tsx
@@ -1,5 +1,7 @@
+import Image from "next/image";
 import Link from "next/link";
 import { useEffect } from "react";
+import logo from "../assets/logo1.png";
 
 export default function FourOhFour(): JSX.Element {
 
@@ -13,9 +15,9 @@ export default function FourOhFour(): JSX.Element {
         <div className="row">
           <div className="middle-content">
             <Link href="/">
-              <img className="md-logo" src="/images/logo1.png" alt="" />
+              <Image src={logo} alt="Before You Play Logo" />
             </Link>
-            <img src="images/uploads/err-img.png" alt="" />
+            {/* <img src="images/uploads/err-img.png" alt="" /> */}
             <h1>Page not found</h1>
             <Link href="/" className="redbtn">
               go home
--- client/src/components/Gamepage/GameImage.tsx
+++ client/src/components/Gamepage/GameImage.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import css from "./GameImage.module.css";
 
 interface Game {
@@ -13,17 +14,25 @@ export default function GameImage(props: GameImageProps) {
   return (
     <>
       {props.game ? (
-        <img
-        src={`https://vgiapitest.blob.core.windows.net/game-images/${props.game.imageName}.webp`}
-        alt={`Cover art for the video game ${props.game.title}`}
-        className={css.img}
-      />
+        <Image
+          src={`https://vgiapitest.blob.core.windows.net/game-images/${props.game.imageName}.webp`}
+          alt={`Cover art for the video game ${props.game.title}`}
+          width={357}
+          height={476}
+          className={css.img}
+          sizes="(min-width: 769px) 18.62vw, 84.62vw"
+        />
       ) : (
-        <img
-        src={`https://vgiapitest.blob.core.windows.net/game-images/byp-new-game.webp`}
-        alt={"Generic cover art which displays when a game is loading or the game has not been reviewed by an admin."}
-        className={css.img}
-      />
+        <Image
+          src={`https://vgiapitest.blob.core.windows.net/game-images/byp-new-game.webp`}
+          alt={
+            "Generic cover art which displays when a game is loading or the game has not been reviewed by an admin."
+          }
+          width={357}
+          height={476}
+          className={css.img}
+          sizes="(min-width: 769px) 18.62vw, 84.62vw"
+        />
       )}
     </>
   );
